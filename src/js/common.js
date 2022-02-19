9c6ae9dfb18aa7c607988faa586563dc841b830a
alpscloud <winde100kg@gmail.com>
2022-02-19 15:58:03 +0200

    added several sections

--- src/js/common.js
+++ src/js/common.js
@@ -17,5 +17,28 @@ $(document).ready(function() {
 		}, 200);
 	});
 
+	if ($('.js-select').length > 0) {
+		$('.js-select').on('click',  function(e) {
+			$(this).toggleClass('is-opened');
+		});
+
+		$('.js-select').on('click', 'li',  function(e) {
+			var value = $(this).attr('data-value');
+			$(this).parents('.js-select').find('input').val(value).trigger('change');
+			
+		});
+
+
+		$(document).mouseup(function (e) {
+			var container = $('.js-select');
+			if (container.has(e.target).length === 0){
+				container.removeClass('is-opened');
+			}
+		});
+	}
+
+
+	$("input[type=tel]").inputmask({"mask": "+380 (99) 999 99 99","clearIncomplete": false});
+
 
 });
